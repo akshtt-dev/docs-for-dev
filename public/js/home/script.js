d03b661b1af6e8224f6eee1d3546f5389b8d4ebc
akshtt-dev <maillegendop@gmail.com>
2024-06-10 16:09:44 +0530

    chore: Refactor CSS variables, implement dark theme toggle, and update file paths and imports

--- public/js/home/script.js
+++ public/js/home/script.js
@@ -1,27 +1,27 @@
 // LIGHT MODE AND DARK MODE TOGGLE
-const darkMode = document.getElementById('darkMode');
-const lightMode = document.getElementById('lightMode');
-const mode = document.getElementById('mode');
+const darkMode = document.getElementById("darkMode");
+const lightMode = document.getElementById("lightMode");
+const mode = document.getElementById("mode");
 
-mode.onclick = function() {
-    document.body.classList.toggle('dark-theme');
-    if (document.body.classList.contains('dark-theme')) {
-        lightMode.style.display = 'none';
-        darkMode.style.display = 'block';
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-        lightMode.style.display = 'block';
-        darkMode.style.display = 'none';
-    }
-}
+mode.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    lightMode.style.display = "none";
+    darkMode.style.display = "block";
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+    lightMode.style.display = "block";
+    darkMode.style.display = "none";
+  }
+};
 
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-theme');
-    lightMode.style.display = 'none';
-    darkMode.style.display = 'block';
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-theme");
+  lightMode.style.display = "none";
+  darkMode.style.display = "block";
 } else {
-    document.body.classList.remove('dark-theme');
-    lightMode.style.display = 'block';
-    darkMode.style.display = 'none';
+  document.body.classList.remove("dark-theme");
+  lightMode.style.display = "block";
+  darkMode.style.display = "none";
 }
